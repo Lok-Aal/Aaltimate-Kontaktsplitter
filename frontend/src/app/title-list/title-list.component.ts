2a23ad835fa2c4d445df3a40733a49327599b201
Raqhael <Raphael.sack@gmx.de>
2023-05-09 22:45:40 +0200

    bug fic

--- frontend/src/app/title-list/title-list.component.ts
+++ frontend/src/app/title-list/title-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { AddTitleDialogComponent } from '../add-title-dialog/add-title-dialog.component';
 import { ContactParserService } from '../contact-parser.service';
@@ -10,12 +10,22 @@ import { ToastServiceService } from '../toast-service.service';
   templateUrl: './title-list.component.html',
   styleUrls: ['./title-list.component.scss']
 })
-export class TitleListComponent {
+export class TitleListComponent implements OnInit {
 
   titles: string[] = [];
   ref: DynamicDialogRef | undefined;
 
   constructor(private dialogService: DialogService, private contactService: ContactParserService, private notificationService: ToastServiceService) { }
+  ngOnInit(): void {
+    this.contactService.getTitles().subscribe({
+      next: (titles: string[]) => {
+        this.titles = titles;
+      },
+      error: (error: any) => {
+        this.notificationService.showError(error); 
+      }
+    });
+  }
 
   onAddTitle(){
     this.showDialog();
@@ -32,7 +42,7 @@ export class TitleListComponent {
     this.ref.onClose.subscribe(({title, gender}) => {
       this.contactService.addTitle(title, gender).subscribe({
         next: (_) => {
-          this.titles = Array.from(new Set([...this.titles, title]));
+          this.titles = Array.from(new Set([title, ...this.titles]));
         },
         error: (error: any) => {
           this.notificationService.showError(error); 
