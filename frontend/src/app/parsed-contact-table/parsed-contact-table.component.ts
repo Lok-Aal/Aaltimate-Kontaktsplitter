c2debc91154c0ce28260e2268f1a48c18439ce8d
Raqhael <Raphael.sack@gmx.de>
2023-05-09 23:18:14 +0200

    final touches

--- frontend/src/app/parsed-contact-table/parsed-contact-table.component.ts
+++ frontend/src/app/parsed-contact-table/parsed-contact-table.component.ts
@@ -3,6 +3,7 @@ import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { AnredeRespone, Contact } from 'src/model/contact';
 import { ContactParserService } from '../contact-parser.service';
 import { FormattedStringDialogComponent } from '../formatted-string-dialog/formatted-string-dialog.component';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-parsed-contact-table',
@@ -12,7 +13,7 @@ import { FormattedStringDialogComponent } from '../formatted-string-dialog/forma
 export class ParsedContactTableComponent {
 
 
-  constructor(public dialogService: DialogService, private contactService: ContactParserService) { 
+  constructor(public dialogService: DialogService, private contactService: ContactParserService, private confirmationService: ConfirmationService) { 
 
   }
   ref: DynamicDialogRef | undefined;
@@ -22,6 +23,22 @@ export class ParsedContactTableComponent {
   editContact(contact: Contact){
     this.onEditContact.emit(contact);
   } 
+
+  deleteContact(contact: Contact, event: Event){
+
+    this.confirmationService.confirm({
+      message: 'Sind Sie sicher, dass Sie den Kontakt löschen wollen?',
+      header: 'Kontakt löschen',
+      icon: 'pi pi-exclamation-triangle',
+      target: event.target!,
+      acceptLabel: 'Ja',
+      rejectLabel: 'Nein',
+      accept: () => {
+        this.contacts = this.contacts.filter((c) => c !== contact);
+      },
+      reject: () => {}
+    });
+  }
 
   generateEmail(contact: Contact){
     this.contactService.generateAnrede(contact).subscribe((anreden: AnredeRespone) => {
